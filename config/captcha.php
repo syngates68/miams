b9ca0ebd712501418fdaf2cb0281ddbed954a93f
Quentin Schifferle <q.schifferle@coprotec.net>
2020-12-07 09:24:00 +0100

    Structure de base

---
+++ config/captcha.php
@@ -0,0 +1,10 @@
+<?php
+function get_public_key()
+{
+    return "6LdWUOIUAAAAAPYUDDSKxhOBpl7GkZ5blVR9tHac";
+}
+
+function get_secret_key()
+{
+    return "6LdWUOIUAAAAABrxmpEBxFoPo-rl-GRjWi5vhvmv";
+}
---
+++ config/fonctions.php
@@ -0,0 +1,21 @@
+<?php
+include('database.php');
+
+function slugify($string, $delimiter = '-') 
+{
+	$oldLocale = setlocale(LC_ALL, '0');
+	setlocale(LC_ALL, 'en_US.UTF-8');
+	$clean = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+	$clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
+	$clean = strtolower($clean);
+	$clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
+	$clean = trim($clean, $delimiter);
+    setlocale(LC_ALL, $oldLocale);
+    
+	return $clean;
+}
+
+function formate_date($date)
+{
+    return date("d/m/Y", strtotime($date)); 
+}
